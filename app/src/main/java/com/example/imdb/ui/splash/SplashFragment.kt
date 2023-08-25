bbcd7abb6293b6c5380824eb0dc4bd7ce2fc455e
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-08-25 16:38:06 +0500

    add splash screen

--- app/src/main/java/com/example/imdb/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/imdb/ui/splash/SplashFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.imdb.R
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +19,18 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadData()
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun loadData(){
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(2000)
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        }
     }
 }
